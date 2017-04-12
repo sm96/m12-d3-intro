38cee09d6b51ba34cba065510bbf4a4d49cdfe88
Sanchya Mahajan <sanchya.mahajan@gmail.com>
2017-04-11 22:03:13 -0700

    added

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -4,12 +4,21 @@
 $(function() {
 
   // Select the element with id `sandbox` and append a `div` element to it. Make sure to store the div in a JavaScript varaible
+  var div = (d3.select('#sandbox')
+  			.append('div')
+  			.attr('id', 'my-div')
+  			.style('fill', 'blue'));
+ 
 
   // Create a variable `mySvg` by appending an `svg` element to your newly created `div`
+  var mySvg = div.append('svg');
 
   // Set both the width and height attributes of your `svg` to 300 
+  mySvg.attr('height', 300).attr('width', 300);
 
   // Append a `circle` element to your `svg`, setting the properties noted in index.html
+  mySvg.append('circle')
+  		.attr('r', 10).attr('cx', 100).attr('cy', 100);
 
   // Append a `rect` element to your `svg`, setting the properties noted in index.html
 
--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -16,10 +16,15 @@ var people2 = [
 $(function() {
   // Create a varaible myDiv by selecting the element with id my-div (this is already created for you).
 
-
+    var myDiv = d3.select('#my-div')
   // Create a variable `paragraphs` by selecting all  `p` elements in  `myDiv`, and binding you data  `people` to it.
   // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
 
+    var paragraphs = myDiv.selectAll('p').data(people, function(person){return person.name});
+    paragraphs.enter()
+    .append('p')
+    .text(function(person){return person.name})
+    .style('color', function(person){return person.favColor});
 
 
   // Add a paragraph to the div for each person in your dataset following instructions in the index.html file
@@ -29,9 +34,15 @@ $(function() {
   setTimeout(function(){
     // Re-define your variable `paragraphs` by selecting all `p` elements in `myDiv`, and binding the data `people2` to it.
     // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
+      var paragraphs = myDiv.selectAll('p').data(people2, function(person){return person.name});
 
 
-    // Determine which people are no longer represented in the data (`exit`), and transition their opacity to 0 and remove them.
+    // Determine which people are no longer represented in the data (`exit`), 
+    //and transition their opacity to 0 and remove them.
+
+    paragraphs.exit();
+    paragraphs.style('opacity', '0');
+    paragraphs.remove();
     
   }, 3000);
 });
